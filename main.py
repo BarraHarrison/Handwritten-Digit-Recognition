028a7244f05943ac6a7dd7cad3e9ddc6343d2170
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-21 16:16:27 +0900

    Img in a list as a numpy array

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
 import tensorflow as tf 
+import os
 
-mnist = tf.keras.datasets.mnist
-# X being the pixel data (image) and Y being the classification (number)
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# mnist = tf.keras.datasets.mnist
+# # X being the pixel data (image) and Y being the classification (number)
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model = tf.keras.models.Sequential()
 
@@ -27,6 +28,10 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.load_model("Handwritten.keras")
 
-loss, accuracy = model.evaluate(x_test, y_test)
-print(loss)
-print(accuracy)
+image_number = 1
+while os.path.isfile(f"digits/digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+    except:
+        pass
