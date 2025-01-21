c7f3cab8cc9c846c7f60280a7f85d7e79ca5e0bc
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-21 16:20:21 +0900

    Prediction using argmax and progression added with the image_number

--- main.py
+++ main.py
@@ -33,5 +33,11 @@ while os.path.isfile(f"digits/digit{image_number}.png"):
     try:
         img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
         img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"This digit is probably a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
     except:
-        pass
+        print("Error!")
+    finally:
+        image_number += 1
