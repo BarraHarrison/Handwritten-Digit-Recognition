238bdf94bf99f3adf746799a08a3eca73d40d0ab
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-21 15:28:53 +0900

    Initialize MNIST dataset, normalize input data and Sequential model structure

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # Handwritten Digit Recognition in Python
-# pip install numpy
-# pip install opencv-python
-# pip install matplotlib
-# pip install tensorflow
+import cv2
+import numpy as np 
+import matplotlib.pyplot as plt 
+import tensorflow as tf 
+
+mnist = tf.keras.datasets.mnist
+# X being the pixel data (image) and Y being the classification (number)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
